8f7bf2441cf562c7437a819510467690a8c18562
AMailosaur <andy@mailosaur.com>
2015-03-19 22:34:58 +0000

    Fix dependencies and increase version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mailosaur',
-      version='0.1',
+      version='0.3',
       description='Package containing python bindings for the mailosaur.com mail testing API.',
       url='http://mailosaur.com/',
       author='Clickity Ltd',
@@ -10,6 +10,7 @@ setup(name='mailosaur',
       license='MIT',
       packages=['mailosaur'],
       install_requires=[
+          'python-dateutil',
           'requests',
           'requests[security]'
       ],
