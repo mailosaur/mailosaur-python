150f753144c13d0d147f79a7c5ff67303a25fad9
Andy Dobbels <andy@mailosaur.com>
2016-05-19 00:07:56 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mailosaur',
-      version='3.0.1',
+      version='3.0.2',
       description='Python client library for Mailosaur',
       url='https://mailosaur.com',
       author='Clickity Ltd',
