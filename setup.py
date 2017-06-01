cced850df75679de3fbdf92856baf060202d9449
Andy Dobbels <andy@mailosaur.com>
2017-06-01 12:16:04 +0100

    Increment version to 3.0.3 which now supports Python 2 and 3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mailosaur',
-      version='3.0.2',
+      version='3.0.3',
       description='Python client library for Mailosaur',
       url='https://mailosaur.com',
       author='Mailosaur Ltd',
