5208c1bd22e5ff08d13be4cfb2e64704f712a795
Jon Mahoney <jon@mailosaur.com>
2018-03-15 22:35:01 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='mailosaur',
-      version='5.0.3',
+      version='5.0.4',
       description='Python client library for Mailosaur',
       url='https://mailosaur.com',
       author='Mailosaur Ltd',
       author_email='code@mailosaur.com',
       keywords='email automation testing selenium robot framework',
       license='MIT',
-      packages=['mailosaur'],
+      packages=find_packages(),
       install_requires=[
           'python-dateutil',
           'requests',
